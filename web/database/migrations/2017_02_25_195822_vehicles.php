09a3659ee3b497fd8ec02cda1b8c954dada0f3d3
mousbah92 <mousbah92@gmailcom>
2017-02-26 15:20:29 +0200

    database update

--- web/database/migrations/2017_02_25_195822_vehicles.php
+++ web/database/migrations/2017_02_25_195822_vehicles.php
@@ -14,7 +14,19 @@ class Vehicles extends Migration
     public function up()
     {
         Schema::create('vehicles', function (Blueprint $table) {
-            $table->increments('id');
+            $table->increments('id')->unique();
+	    $table->integer('plate_number');
+	    $table->string('plate_code');
+	    $table->string('brand');
+	    $table->string('model');
+	    $table->year('production_year');
+	    $table->string('color');
+	    $table->string('chassis_number');
+	    $table->string('engine_number');
+	    $table->string('aquisition_date');
+	    $table->string('type');
+	    $table->string('operation_year');
+	    $table->integer('user_id');
             $table->timestamps();
         });
     }
