3dfbab7d805121e7c0c69a06b88a4cca43d48262
Hassan Salem <h.salem7788@gmail.com>
2017-02-26 22:33:33 +0200

    create line index list

---
+++ web/resources/views/lines/index.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+		<div class="panel-heading">Lines 
+		<a href="{{route('line.create')}}" class="btn btn-link"><i class="fa fa-plus"></i></a></div>
+
+                <div class="panel-body">
+		    <table class="table table-striped">
+			<thead>
+			  <tr>
+			    <th>Line Number</th>
+			    <th>Station Name</th>
+			    <th>Actions</th>
+			  </tr>
+			</thead>
+			<tbody>
+			@foreach($lines as $line)
+			  <tr>
+			      <td>{{$line->number}}</td>
+			      <td>{{$line->station->name}}</td>
+			      <td>
+				<a href="{{route('line.edit', ['id' => $line->id])}}" class="btn btn-success"> Edit </a>
+				<form action="{{route('line.delete')}}" method="POST">
+				    {{csrf_field()}}
+				    <input type="hidden" name="_method" value="DELETE" />
+				    <input type="hidden" name="id" value="{{$line->id}}" />
+				    <input type="submit" value="DELETE" class="btn btn-danger" />
+				</form>
+			      </td>
+			  </tr>
+			@endforeach
+			</tbody>
+		    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
