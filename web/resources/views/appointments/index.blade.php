18154398e4292fedd2a86eab2c3fba2f510c36dd
Hassan Salem <h.salem7788@gmail.com>
2017-03-06 20:53:14 +0200

    use glyph icon

--- web/resources/views/appointments/index.blade.php
+++ web/resources/views/appointments/index.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Appointment <a href="{{route('appointment.create')}}" class="btn btn-link"><i class="fa fa-plus"></i></a></div>
+                <div class="panel-heading">Appointment <a href="{{route('appointment.create')}}" class="btn btn-link"><i class="glyphicon glyphicon-plus"></i></a></div>
 
                 <div class="panel-body">
 		    <table class="table table-striped">
