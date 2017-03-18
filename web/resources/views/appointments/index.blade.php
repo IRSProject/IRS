a3825dbb2e36c951ab6693b21fb3fe100eee5f23
itsned911 <darazinasreddine@gmail.com>
2017-03-18 12:02:21 +0200

    edit

--- web/resources/views/appointments/index.blade.php
+++ web/resources/views/appointments/index.blade.php
@@ -25,7 +25,7 @@
 			      <td>
 				
 				<form action="{{route('appointment.delete', ['id' => $appointment->id, 'station_id' => $appointment->station->id])}}" method="POST">
-				<a href="{{route('appointment.edit', ['id' => $appointment->id])}}" class="btn btn-success"> Edit </a>
+					<a href="{{route('appointment.edit', ['id' => $appointment->id])}}" class="btn btn-success"> Edit </a>
 				    {{csrf_field()}}
 				    <input type="hidden" name="_method" value="DELETE" />
 				    <input type="hidden" name="id" value="{{$appointment->id}}" />
