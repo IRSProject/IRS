b5115aaff1ac3728e81f3de2af413ca71f98bbaa
itsned911 <darazinasreddine@gmail.com>
2017-03-30 14:46:02 +0300

    created database vehiclecheck

---
+++ web/database/migrations/2017_03_30_110730_vehiclecheck.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Vehiclecheck extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+      Schema::create('vehiclecheck', function (Blueprint $table) {
+          $table->increments('id')->unique();
+          $table->integer('plate_number');
+          $table->string('plate_code');
+          $table->string('brand');
+          $table->string('model');
+          $table->integer('production_year');
+          $table->string('color');
+          $table->string('chassis_number');
+          $table->string('engine_number');
+          $table->date('aquisition_date');
+          $table->string('type');
+          $table->string('operation_year');
+          $table->timestamps();
+      });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+      Schema::dropIfExists('vehiclecheck');
+  }
+}
