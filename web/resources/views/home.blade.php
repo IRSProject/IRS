3155666406e002081a031788cef3ec597c058a3e
mousbah92 <mousbah92@gmail.com>
2017-05-14 11:53:05 +0300

    home.blade

--- web/resources/views/home.blade.php
+++ web/resources/views/home.blade.php
@@ -103,7 +103,7 @@
             </div>
 
             <div class="row">
-              <div class="col-lg-8">
+              <div class="col-lg-12">
                   <!-- /.panel -->
                   <div class="panel panel-default">
                       <div class="panel-heading">How to use</div>
@@ -115,12 +115,21 @@
                                   </div>
                                   <div class="timeline-panel">
                                       <div class="timeline-heading">
-                                          <h4 class="timeline-title">Add your vehicle</h4>
-                                          <p><small class="text-muted"><i class="fa fa-clock-o"></i> 11 hours ago via Twitter</small>
-                                          </p>
+                                          <h4 class="timeline-title">Add new vehicle</h4>
                                       </div>
                                       <div class="timeline-body">
-                                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero laboriosam dolor perspiciatis omnis exercitationem. Beatae, officia pariatur? Est cum veniam excepturi. Maiores praesentium, porro voluptas suscipit facere rem dicta, debitis.</p>
+                                          <p>
+                                            1- First click on the vehicles button up in the navigation bar.
+                                          </p>
+                                          <p>
+                                            2- Then click on <i class="glyphicon glyphicon-plus" color="blue"></i>.
+                                          </p>
+                                          <p>
+                                            3- Fill all the information required about your vehicle.
+                                          </p>
+                                          <p>
+                                            4- Click <input type="submit" value="Save" class="btn btn-success" />.
+                                          </p>
                                       </div>
                                   </div>
                               </li>
@@ -129,11 +138,15 @@
                                   </div>
                                   <div class="timeline-panel">
                                       <div class="timeline-heading">
-                                          <h4 class="timeline-title">Know what station you want to book your appointment in</h4>
+                                          <h4 class="timeline-title">Choose a station</h4>
                                       </div>
                                       <div class="timeline-body">
-                                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem dolorem quibusdam, tenetur commodi provident cumque magni voluptatem libero, quis rerum. Fugiat esse debitis optio, tempore. Animi officiis alias, officia repellendus.</p>
-                                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium maiores odit qui est tempora eos, nostrum provident explicabo dignissimos debitis vel! Adipisci eius voluptates, ad aut recusandae minus eaque facere.</p>
+                                          <p>
+                                            1- First click on the stations button up in the navigation bar.
+                                          </p>
+                                          <p>
+                                            2- Choose the station with the nearest address.
+                                          </p>
                                       </div>
                                   </div>
                               </li>
@@ -145,17 +158,31 @@
                                           <h4 class="timeline-title">Generate appointment</h4>
                                       </div>
                                       <div class="timeline-body">
-                                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus numquam facilis enim eaque, tenetur nam id qui vel velit similique nihil iure molestias aliquam, voluptatem totam quaerat, magni commodi quisquam.</p>
+                                          <p>
+                                            1- First click on the appointments button up in the navigation bar.
+                                          </p>
+                                          <p>
+                                            2- Click on <i class="glyphicon glyphicon-refresh"></i>.
+                                          </p>
+                                          <p>
+                                            3- Fill all the information needed.
+                                          </p>
+                                          <p>
+                                            4- Click on <input type="submit" value="Generate" class="btn btn-warning" />.
+                                          </p>
                                       </div>
                                   </div>
                               </li>
                               <li class="timeline-inverted">
                                   <div class="timeline-panel">
                                       <div class="timeline-heading">
-                                          <h4 class="timeline-title">Pick a date & time</h4>
+                                          <h4 class="timeline-title">Choose appointment</h4>
                                       </div>
                                       <div class="timeline-body">
-                                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates est quaerat asperiores sapiente, eligendi, nihil. Itaque quos, alias sapiente rerum quas odit! Aperiam officiis quidem delectus libero, omnis ut debitis!</p>
+                                          <p>
+                                            Click on <button class="btn btn-success btn-xs glyphicon glyphicon-ok" data-title="Delete" data-toggle="modal" data-target="#delete"></button> that is on the right of your chosen appointment.
+                                          </p>
+                                          </p>
                                       </div>
                                   </div>
                               </li>
@@ -167,7 +194,7 @@
                                           <h4 class="timeline-title">Appointment reserved</h4>
                                       </div>
                                       <div class="timeline-body">
-                                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis minus modi quam ipsum alias at est molestiae excepturi delectus nesciunt, quibusdam debitis amet, beatae consequuntur impedit nulla qui! Laborum, atque.</p>
+                                          <p>Congratulations, your appointment has been reserved. You can either check it by clicking on <i class="glyphicon glyphicon-list"></i> or check for it in the calender.</p>
                                       </div>
                                   </div>
                               </li>
